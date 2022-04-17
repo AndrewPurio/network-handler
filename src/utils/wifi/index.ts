26a286efba8412ee070b2a7d9908711aede93f40
Andrew <john@existtribe.com>
2022-04-18 07:59:00 +0800

    Minor scan update

--- src/utils/wifi/index.ts
+++ src/utils/wifi/index.ts
@@ -27,7 +27,7 @@ export const getWlanStatus = async () => {
 export const scanWifi = async () => {
     try {
         await execute("sudo ifconfig wlan0 up")
-        const { stdout, stderr } = await execute("sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID'")
+        const { stdout, stderr } = await execute("sudo iwlist wlan0 scan | egrep 'Cell |Encryption|Quality|Last beacon|ESSID'")
         const wifiStrData = stdout as string
         const wifiDataParser = /Cell \d+ - Address: (\w{2}:?)+\n +Quality=\d{2}\/\d{2}  Signal level=[\w- ]+\n +Encryption key:\w{2,3}\n +ESSID:".+"\n +Extra: Last beacon: \d+ms ago/g
         const patterns = {
